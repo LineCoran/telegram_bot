d667b55b9e81970dba68c828248a4ef6031941f5
Алексей Кузьмичев <kuzmichev.alexey@mpstats.io>
2024-12-13 21:49:43 +0300

    fix failed deploy

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import TelegramBot from 'node-telegram-bot-api';
 // replace the value below with the Telegram token you receive from @BotFather
 const main_token = '8180507441:AAGR3GKel7fus30OPlj2UQ_viJuxnKFASWA';
 
-// const dev_token = '8109563932:AAF2UdGE3-0FR8O2USCaTQwML3aEinWZkIY'
+// const main_token = '8109563932:AAF2UdGE3-0FR8O2USCaTQwML3aEinWZkIY'
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(main_token, {polling: true});
+const bot = new TelegramBot(main_token, { polling: true });
 
 const DEFAULT_CATEGORY = {
     home: 'Квартира',
@@ -40,20 +40,10 @@ bot.setMyCommands([
 const getCategories = (chatId) => {
     const inline_keyboard = [];
 
-    // [{ text: 'Квартира', callback_data: 'home' }],
-    // [{ text: 'Здоровье', callback_data: 'health' }],
-    // [{ text: 'Еда', callback_data: 'food' }],
+    if (Object.keys(DEFAULT_CATEGORY).length === currentChat[chatId].length) {
+        return null
+    }
 
-    // if (currentChat[chatId].categories.length === 0) {
-    //
-    //     console.log('here');
-    //
-    //     Object.keys(DEFAULT_CATEGORY).forEach(callback_data => {
-    //         const text = DEFAULT_CATEGORY[callback_data];
-    //         inline_keyboard.push([{ text, callback_data }]);
-    //     })
-    //
-    // } else {
         Object.keys(DEFAULT_CATEGORY).forEach(callback_data => {
             const text = DEFAULT_CATEGORY[callback_data];
 
@@ -64,7 +54,6 @@ const getCategories = (chatId) => {
                 }]);
             }
         })
-    // }
 
 
     return {
@@ -85,7 +74,15 @@ bot.on('message', async msg => {
         if (!currentChat[chatId]) currentChat[chatId] = getBaseChatInfo()
         if (text === '/start') {
             await bot.sendMessage(chatId, 'Привет, это бот который помогает тебе вести свои финансовые расходы. Для начала работы тебе необходимо настроить свои категории.')
-            return await bot.sendMessage(chatId, 'Выбери из предложенных', getCategories(chatId))
+
+            const categories = getCategories(chatId);
+
+            if (categories) {
+                return await bot.sendMessage(chatId, 'Выбери из предложенных', getCategories(chatId))
+            } else {
+                return await bot.sendMessage(chatId, 'Категорий больше нет')
+            }
+
         };
         if (text === '/info') return bot.sendMessage(chatId, `Тебя зовут ${msg.chat.first_name} ${msg.chat.last_name}`);
         if (text === '/set') return bot.sendMessage(chatId, 'Введи сумму:')
