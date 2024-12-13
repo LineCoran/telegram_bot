ebe585076941c8cf6d7cd5ce51b76de0c1d39b27
Алексей Кузьмичев <kuzmichev.alexey@mpstats.io>
2024-12-13 16:16:23 +0300

    add categories

--- index.js
+++ index.js
@@ -1,51 +1,106 @@
 import TelegramBot from 'node-telegram-bot-api';
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '8180507441:AAGR3GKel7fus30OPlj2UQ_viJuxnKFASWA';
+const main_token = '8180507441:AAGR3GKel7fus30OPlj2UQ_viJuxnKFASWA';
+
+// const dev_token = '8109563932:AAF2UdGE3-0FR8O2USCaTQwML3aEinWZkIY'
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(main_token, {polling: true});
 
-// bot.setMyCommands([
-//     {
-//         command: '/info',
-//         description: 'Расскажи обо мне',
-//     },
-//     {
-//         command: '/start',
-//         description: 'Начать',
-//     }
-// ])
+const DEFAULT_CATEGORY = {
+    home: 'Квартира',
+    health: 'Здоровье',
+    food: 'Еда',
+}
 
-bot.on('message', msg => {
+const currentChat = {};
+
+const getBaseChatInfo = () => {
+    return {
+        categories: [],
+    }
+}
+
+bot.setMyCommands([
+    {
+        command: '/info',
+        description: 'Расскажи обо мне',
+    },
+    {
+        command: '/set',
+        description: 'Внести трату',
+    },
+    {
+        command: '/start',
+        description: 'Начать',
+    }
+])
+
+const getCategories = (chatId) => {
+    const inline_keyboard = [];
+
+    // [{ text: 'Квартира', callback_data: 'home' }],
+    // [{ text: 'Здоровье', callback_data: 'health' }],
+    // [{ text: 'Еда', callback_data: 'food' }],
+
+    // if (currentChat[chatId].categories.length === 0) {
+    //
+    //     console.log('here');
+    //
+    //     Object.keys(DEFAULT_CATEGORY).forEach(callback_data => {
+    //         const text = DEFAULT_CATEGORY[callback_data];
+    //         inline_keyboard.push([{ text, callback_data }]);
+    //     })
+    //
+    // } else {
+        Object.keys(DEFAULT_CATEGORY).forEach(callback_data => {
+            const text = DEFAULT_CATEGORY[callback_data];
+
+            if (!currentChat[chatId].categories.includes(callback_data)) {
+                inline_keyboard.push([{
+                    text,
+                    callback_data
+                }]);
+            }
+        })
+    // }
 
 
+    return {
+        reply_markup: JSON.stringify({ inline_keyboard })
+    }
+}
+
+
+bot.on('message', async msg => {
     try {
+        const chatId = msg.chat.id
+        const text = msg.text
 
-        console.log('msg', msg)
+        // return bot.sendMessage(chatId, 'Я тебя не понял))))')
 
+        console.log(currentChat)
 
-    const chatId = msg.chat.id
-    const text = msg.text
-
-    if (text.toLowerCase() === 'ты пидор') {
-        return bot.sendMessage(chatId, 'Сам ты пидор, понял?!')
-    }
-
-    if (text === '/start') {
-        return bot.sendMessage(chatId, 'Привет Надежда! Это самая первая версия бота от твоего мужа!!!')
-    }
-
-    if (text === '/info') {
-        return bot.sendMessage(chatId, `Тебя зовут ${msg.chat.first_name} ${msg.chat.last_name}`);
-    }
-
-    return bot.sendMessage(chatId, 'Я тебя не понял))))')
+        if (!currentChat[chatId]) currentChat[chatId] = getBaseChatInfo()
+        if (text === '/start') {
+            await bot.sendMessage(chatId, 'Привет, это бот который помогает тебе вести свои финансовые расходы. Для начала работы тебе необходимо настроить свои категории.')
+            return await bot.sendMessage(chatId, 'Выбери из предложенных', getCategories(chatId))
+        };
+        if (text === '/info') return bot.sendMessage(chatId, `Тебя зовут ${msg.chat.first_name} ${msg.chat.last_name}`);
+        if (text === '/set') return bot.sendMessage(chatId, 'Введи сумму:')
 
     } catch (e) {
         console.log(e)
     }
-
-
 })
 
+
+bot.on('callback_query', msg => {
+
+    const chatId = msg.message.chat.id
+    const key = msg.data
+    if (!currentChat[chatId]) return bot.sendMessage(chatId, 'Произошла ошибка');
+    currentChat[chatId].categories.push(key);
+    return bot.sendMessage(chatId, `Ты выбрал "${DEFAULT_CATEGORY[key]}". Еще?`, getCategories(chatId))
+})
